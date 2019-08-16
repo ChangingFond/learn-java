47dd2af7277907f6251c2a429cc725bee0d27ec2
ChangingFond <782773117@qq.com>
2019-08-16 21:55:33 +0800

    Feat: ThreadPool

---
+++ src/main/java/concurrent/TheadPool.java
@@ -0,0 +1,60 @@
+package concurrent;
+
+import java.util.concurrent.*;
+
+/**
+ * @auther: fangchengjin_sx
+ * @date: 2019/8/16 21:28
+ * @description: 四种常见的线程池
+ */
+public class TheadPool {
+
+    private static int produceTaskSleepTime = 400;
+    private static int produceTaskMaxNumber = 10;
+
+    public static void main(String[] args) {
+        ExecutorService threadPool = new ThreadPoolExecutor(2, 4, 3,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(3), new ThreadPoolExecutor.DiscardOldestPolicy());
+
+        for (int i = 0; i < produceTaskMaxNumber; i++) {
+            try {
+                String taskName = "task@" + (i + 1);
+                threadPool.submit(new Task(taskName));
+                Thread.sleep(produceTaskSleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+         fixedThreadPool.submit(new Runnable() {
+             public void run() {
+                 System.out.println(Thread.currentThread().getName());
+             }
+         });
+        }
+    }
+}
+
+class Task implements Runnable {
+
+    private static int consumeTaskSleepTime = 3000;
+
+    private String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public void run() {
+        System.out.println(name);
+        System.out.println(Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(consumeTaskSleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
