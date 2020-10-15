c3661be0541caacf0d80c7d7773c2fc8f0787032
fangchengjin <fangchengjin@xiaomi.com>
2020-10-15 21:38:24 +0800

    feat: jetty connector

--- java-jetty/src/main/java/com/changingfond/jetty/Jetty.java
+++ java-jetty/src/main/java/com/changingfond/jetty/Jetty.java
@@ -1,12 +1,22 @@
 package com.changingfond.jetty;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.NetworkTrafficServerConnector;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.BlockingArrayQueue;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class Jetty {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(31100);
+        Server server = initJettyServer();
 
         ServletContextHandler contextHandler = new ServletContextHandler(
                 ServletContextHandler.SESSIONS
@@ -19,6 +29,28 @@ public class Jetty {
 
         server.start();
         server.join();
+    }
+
+    private static Server initJettyServer() {
+        QueuedThreadPool queuedThreadPool = new QueuedThreadPool(
+            10, 10, 60000, new BlockingArrayQueue(10000));
+        HttpConfiguration httpConfiguration = new HttpConfiguration();
+        httpConfiguration.setRequestHeaderSize(20480);
+
+        Server server = new Server(queuedThreadPool);
+        Executor executors = Executors.newFixedThreadPool(3);
+        ServerConnector serverConnector = new NetworkTrafficServerConnector(server, executors, null, null, 1, 2,
+            new HttpConnectionFactory(httpConfiguration));
+        // 如果不提供 executors 参数，acceptors 和 selectors 需要的线程会和 workers 线程共享线程池，
+        // 最终实际运行的 workers 线程数将会偏小。
+        // ServerConnector serverConnector = new ServerConnector(
+        // server, new HttpConnectionFactory(httpConfiguration));
+        serverConnector.setHost("0.0.0.0");
+        serverConnector.setPort(31100);
+        serverConnector.setAcceptQueueSize(10000);
+        server.setConnectors(new Connector[] { serverConnector });
+
+        return server;
     }
 
 }
