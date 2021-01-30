735ae5488ebb3da9c15a077d00d3ee5045ef9033
fangchengjin <fangchengjin@xiaomi.com>
2021-01-30 23:14:59 +0800

    feat: add netty

--- java-netty/src/main/java/com/changingfond/netty/DiscardServerHandler.java
+++ java-netty/src/main/java/com/changingfond/netty/DiscardServerHandler.java
@@ -71,6 +71,7 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
   @Override
   public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
     // Close the connection when an exception is raised.
+
     cause.printStackTrace();
     ctx.close();
   }
